f8e4181411fda2227657150ce16169c7753fd4b7
John Andrés <johnandrescv@gmail.com>
2020-12-05 12:17:16 -0500

    Agregamos variables de entorno

--- php/mail.php
+++ php/mail.php
@@ -384,7 +384,7 @@
         $mail->SMTPDebug = 0;                                 // Enable verbose debug output
         $mail->Host = 'smtp.gmail.com';                              // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                                // Enable SMTP authentication
-        $mail->Username = 'lacasadelcarretonecuador@gmail.com';                   // SMTP username
+        $mail->Username = getenv('contactmail');                   // SMTP username
         $mail->Password = 'lacasadelcarreton2020';                             // SMTP password
         $mail->SMTPSecure = 'tls';                              // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 587;                                      // TCP port to connect to
